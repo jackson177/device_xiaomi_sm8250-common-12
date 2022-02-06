33f90afd41498f7271e8658e6ab7ad9a583428a3
Abhishek Aggarwal <warabhishek@gmail.com>
2022-02-06 15:39:50 +0100

    sm8250-common: Implement popup camera

--- kona.mk
+++ kona.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Deviceparts
+PRODUCT_PACKAGES += \
+    DeviceParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
